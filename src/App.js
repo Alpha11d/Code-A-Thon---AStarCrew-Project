4416087379752d085faef5a4be94dadeb1183ce8
Divyansh Singh <divyansh.jmdev@gmail.com>
2023-02-25 10:45:41 +0530

    updated interconnections & deps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
+import Category from './pages/Category';
+import PostDetail from './pages/PostDetail';
+import UserDetail from './pages/UserDetail';
+import { fetchCategories } from './utils/api';
 
 function App() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetchCategories().then((data) => {
+      setCategories(data);
+      // console.log(data)
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <Sidebar categories={categories} />
+            <Routes>
+              <Route exact path="/" element={<Home/>} />
+              <Route path="/category/:categoryId" element={<Category/>} />
+              <Route path="/post/:postId" element={<PostDetail/>} />
+              <Route path="/user/:userId" element={<UserDetail/>} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
